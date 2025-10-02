2af25ad82f3f7d72db618771fe260c52ef453584
Atomic <atomicisnoah.code@gmail.com>
2025-10-02 17:38:31 +0100

    first commit

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Slot } from 'expo-router'
+
+export default function _layout() {
+  return (
+    <SafeAreaView>
+      
+      <Text>Auth_layout</Text>
+    <Slot/>
+    </SafeAreaView>
+    
+  )
+}
